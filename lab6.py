45266a20cdb63d907d211daa61f1b069577132cb
Kyle Bello <bellokp2002@gmail.com>
2023-02-16 09:36:10 -0500

    Capitalized second name

--- lab6.py
+++ lab6.py
@@ -1,4 +1,3 @@
 name1 = input("Enter name 1: ")
 name2 = input("Enter name 2: ")
-name2.capitalize
-print("Name 1: " + name1 + " Name 2: " + name2)
+print("Name 1: " + name1 + " Name 2: " + name2.capitalize())
